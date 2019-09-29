32d5459fd6109e9fef28c887657acd331983845a
Ajeo <805574351@qq.com>
2019-09-29 09:37:02 +0800

    09.29

--- src/view/admin/userAdmin/userAdmin.vue
+++ src/view/admin/userAdmin/userAdmin.vue
@@ -9,9 +9,11 @@
     <div class="goods-table">
       <template>
         <el-table :data="tableData" style="width: 100%">
-          <el-table-column prop="date" label="日期" width="180"></el-table-column>
-          <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-          <el-table-column prop="address" label="地址"></el-table-column>
+          <el-table-column prop="user_account" label="账号" width="180"></el-table-column>
+          <el-table-column prop="real_name" label="姓名" width="180"></el-table-column>
+          <el-table-column prop="insert" label="地址"></el-table-column>
+          <el-table-column prop="email" label="邮箱"></el-table-column>
+          <el-table-column prop="create_time" label="创建时间"></el-table-column>
         </el-table>
       </template>
     </div>
@@ -28,29 +30,43 @@ export default {
     return {
       tableData: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
+          user_id:1,
+          user_account: "",
+          pw:"",
+          nick_name: "",
+          real_name:"",
+          sex: 1,
+          email: "1111@qq.com",
+          insert: "",
+          user_type:1,
+          credit: "88.0",
+          qq:"",
+          create_time:"",
+          last_login_time:""
         }
       ]
-    };
+    }
+  },
+  created(){
+    this.getuserlist();
+  },
+  methods:{
+    getuserlist(){
+      this.http.service.post('/user/allUserInfo',{}).then(
+                (res)=>{
+                    console.log(res.data);
+                    this.tableData = res.data;
+                }
+            )
+            .catch(
+                (res)=>{
+                    console.log(res)
+                }
+            )
+    }
   }
-};
+  
+}
 </script>
 
 
